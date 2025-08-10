a325a7c5d3fa995ac1888f8c9bc31edaccbfcbce
DheeD <zhulanoff.ilia@yandex.ru>
2025-08-10 17:22:29 +0400

    Created variables for sending to another page

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,20 +1,59 @@
-<script  lang="ts">
+<script lang="ts">
 export default {
   data() {
     return {
-      message: 'Hello Vue!'
-    };
+      userInput: [String, Date],
+      text: new String(),
+      date: new Date(),
+    }
+  },
+  methods: {
+    insertDate(value: Date) {
+      this.date = value;
+    },
+    insertText(value: String) {
+      this.text = value;
+    },
+    sendData() {
+      // Assuming you want to navigate to another page with the data
+      this.$router.push({
+        name: 'NextPage', // Replace with your actual route name
+        query: {
+          text: this.text,
+          date: this.date.toISOString() // Convert date to string if needed
+        }
+      });
+      // You can also use this.$emit('dataSent', { text: this.text, date: this.date }) 
+      // if you want to emit an event instead of navigating
+    }
   }
 }
 </script>
 
 <template>
   <header>
-   
   </header>
 
   <main>
-   
+   <!-- <p>{{ text }}</p>
+   <p>Today's date is: {{ date }}</p> -->
+
+   <form>
+      <input type="text"
+        v-model="text" 
+        placeholder="text" />
+    <p>
+      <input
+        type="date"
+        v-model="date"
+        placeholder="date" />
+    </p>
+    <p>
+      <button @click="sendData()" type="button">
+        Go to next page
+      </button>
+    </p>
+   </form>
   </main>
 </template>
 
--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -1,4 +1,5 @@
-import './assets/main.css'
+//import './assets/main.css'
+import { createMemoryHistory, createRouter } from 'vue-router'
 
 import { createApp } from 'vue'
 import App from './App.vue'
